ebe59772201389c902fdc81ae18a491cb42824b2
Nonthakornn <ext.nonthakorn.che@truedigital.com>
2024-07-04 21:31:50 +0700

    04/07/2024

--- JS/21-Merge_Two_Sorted_Lists.js
+++ JS/21-Merge_Two_Sorted_Lists.js
@@ -15,7 +15,7 @@ var mergeTwoLists = function(list1, list2) {
     while(list1 && list2) {
         if(list1.val < list2.val) {
             curr.next = list1;
-            list1 = list1.next;
+            list1 = list1.next; 
         } else {
             curr.next = list2
             list2 = list2.next;
